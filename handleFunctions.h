33db6a08b0153bfad7754b454a83d06439124adf
David <davidaristeed@gmail.com>
2022-03-23 06:56:20 +0300

    Update handleFunctions.h

--- handleFunctions.h
+++ handleFunctions.h
@@ -9,14 +9,26 @@ void handleRoot()
 //---------------------------------------
 void handlePOT()
 {
- Serial.println("Sending portVal");
+ Serial.println("Sending temp");
+ String POTval = String(analogRead(A0));
+ server.send(200, "text/plane", POTval);
+}
+void handleWater()
+{
+ Serial.println("Sending Water level");
+ String POTval = String(analogRead(A0));
+ server.send(200, "text/plane", POTval);
+}
+void handlePower()
+{
+ Serial.println("Sending Power Level");
  String POTval = String(analogRead(A0));
  server.send(200, "text/plane", POTval);
 }
 //----------------------------------------
 void handleLight()
 {
-    Serial.println("Lights");
+    Serial.println("Light function");
     l_state = !l_state;
     String state;
     if(l_state)
@@ -28,4 +40,31 @@ void handleLight()
     }
     server.send(200,"text/plain",state);
 }
-
+void handlePump()
+{
+    Serial.println("Pump function");
+    p_state = !p_state;
+    String state;
+    if(p_state)
+    {
+        state = "ON";
+    }
+    else{
+        state = "OFF";
+    }
+    server.send(200,"text/plain",state);
+}
+void handleFan()
+{
+    Serial.println("Fan function");
+    f_state = !f_state;
+    String state;
+    if(f_state)
+    {
+        state = "ON";
+    }
+    else{
+        state = "OFF";
+    }
+    server.send(200,"text/plain",state);
+}
